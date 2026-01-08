aac591d1317e1e337ed6e663857008aea9b1197f
Cornet Benjamin <benjamincornet62130@gmail.com>
2026-01-08 17:10:28 +0100

    changement sprite

--- Dino Game/Assets/Scripts/Player.cs
+++ Dino Game/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float jumpForce = 10f;
+    public float jumpForce = 8f;
     private Rigidbody2D rb;
     private bool isGrounded = true;
 
